35f2f947352aa265be3ab5605ae078b719eb31ff
Gloria Labory <glabory@gmail.com>
2025-08-12 17:11:06 +0200

    Añadir eslint al proyecto

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+// ESLint Flat Config para Vue 3 + JavaScript ESM
+// Ejecuta: npm run lint  |  npm run lint:fix
+
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+
+export default [
+  // Ignorar carpetas generadas
+  { ignores: ['dist/**', 'node_modules/**', 'public/**'] },
+
+  // Reglas JS recomendadas
+  js.configs.recommended,
+
+  // Config recomendada de Vue con parser para .vue
+  ...pluginVue.configs['flat/recommended'],
+
+  // Ajustes del proyecto + reglas personalizadas
+  {
+    files: ['**/*.{js,vue}'],
+    languageOptions: {
+      parser: vueParser,
+      ecmaVersion: 2023,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaVersion: 2023,
+        sourceType: 'module',
+      },
+    },
+    plugins: { vue: pluginVue },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+  // Estilo: sin punto y coma
+  'semi': ['error', 'never'],
+  // Espacios tras palabras clave (if (), for (), etc.)
+  'keyword-spacing': ['error', { before: true, after: true }],
+  // Espacios alrededor de operadores infijos (=, +, -, etc.)
+  'space-infix-ops': ['error', { int32Hint: false }],
+      'vue/no-mutating-props': 'error',
+      'vue/no-unused-vars': 'warn',
+  'vue/no-v-html': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/html-self-closing': ['warn', { html: { void: 'always' } }],
+    },
+  },
+]
